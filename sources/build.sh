07936e5fd354b66b539ca18560b5a4e8ffbbabcf
Denis Moyogo Jacquerye <moyogo@gmail.com>
2020-08-24 14:39:40 +0100

    Update build.sh

--- sources/build.sh
+++ sources/build.sh
@@ -16,8 +16,8 @@ fontmake -m LibreBaskerville.designspace -i -o ttf --output-dir ../fonts/ttf/
 fontmake -m LibreBaskerville.designspace -i -o otf --output-dir ../fonts/otf/
 
 echo "Generating VFs"
-mkdir -p ../fonts/vf
-fontmake -m LibreBaskerville.designspace -o variable --output-path ../fonts/vf/LibreBaskerville[wght].ttf
+mkdir -p ../fonts/variable
+fontmake -m LibreBaskerville.designspace -o variable --output-path ../fonts/variable/LibreBaskerville[wght].ttf
 
 
 
@@ -29,21 +29,18 @@ ttfs=$(ls ../fonts/ttf/*.ttf)
 for ttf in $ttfs
 do
 	gftools fix-dsig -f $ttf;
-	ttfautohint $ttf "$ttf.fix";
+	python3 -m ttfautohint $ttf "$ttf.fix";
 	mv "$ttf.fix" $ttf;
 done
 
-vfs=$(ls ../fonts/vf/*\[wght\].ttf)
+vfs=$(ls ../fonts/variable/*\[wght\].ttf)
 
 echo "Post processing VFs"
 for vf in $vfs
 do
 	gftools fix-dsig -f $vf;
-	ttfautohint-vf --stem-width-mode nnn $vf "$vf.fix";
 	mv "$vf.fix" $vf;
 done
-
-
 
 echo "Fixing VF Meta"
 gftools fix-vf-meta $vfs;
@@ -54,20 +51,28 @@ do
 	mv "$vf.fix" $vf;
 	ttx -f -x "MVAR" $vf; # Drop MVAR. Table has issue in DW
 	rtrip=$(basename -s .ttf $vf)
-	new_file=../fonts/vf/$rtrip.ttx;
+	new_file=../fonts/variable/$rtrip.ttx;
 	rm $vf;
 	ttx $new_file
 	rm $new_file
 done
 
-echo "Fixing Hinting"
+echo "Fixing Non-Hinting"
 for vf in $vfs
 do
-	gftools fix-hinting $vf;
+	gftools fix-nonhinting $vf $vf;
 	mv "$vf.fix" $vf;
 done
+echo "Fixing Hinting"
 for ttf in $ttfs
 do
 	gftools fix-hinting $ttf;
 	mv "$ttf.fix" $ttf;
 done
+
+rm -f ../fonts/variable/*.ttx
+rm -f ../fonts/ttf/*.ttx
+rm -f ../fonts/variable/*gasp.ttf
+rm -f ../fonts/ttf/*gasp.ttf
+
+echo "Done"
